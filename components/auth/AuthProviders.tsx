02ae054c689b16e7b233cd0cda0897379bd39575
Abdullah-142 <abdulah14200@gmail.com>
2024-05-23 05:41:42 -0700

    chore: Update AuthProviders component to use react's useTransition hook
    The code changes in the `AuthProviders.tsx` file update the usage of the `useState` hook to the `useTransition` hook from React. This change improves the performance and user experience of the AuthProviders component.

--- components/auth/AuthProviders.tsx
+++ components/auth/AuthProviders.tsx
@@ -1,37 +1,34 @@
 "use client";
 
 import { createBroswerClient } from "@/lib/supabase/client";
-import { useState } from "react";
+import { useTransition } from "react";
 import { Button } from "../ui/button";
 
 const AuthProviders = () => {
-   const [loading, setLoading] = useState(false);
-   const [providers, setProviders] = useState<"github" | "google" | null>(null);
-
+   const [isPendng, startTransition] = useTransition();
    const handleLogin = (provider: "github" | "google") => {
       try {
-         setLoading(true);
-         const supabase = createBroswerClient();
-         supabase.auth.signInWithOAuth({
-            provider: provider,
-            options: {
-               //redirecting to the callback route
+         startTransition(() => {
+            const supabase = createBroswerClient();
+            supabase.auth.signInWithOAuth({
+               provider: provider,
+               options: {
+                  //redirecting to the callback route
 
-               redirectTo: `${location.origin}/callback`,
-            },
+                  redirectTo: `${location.origin}/callback`,
+               },
+            });
          });
-         setLoading(false);
       } catch (error) {
          console.log("error", error);
-         setLoading(false);
       }
    };
    return (
       <div className="flex gap-3 flex-wrap justify-between">
-         <Button onClick={() => handleLogin("github")}>
+         <Button disabled={isPendng} onClick={() => handleLogin("github")}>
             Sign in with Github
          </Button>
-         <Button onClick={() => handleLogin("google")}>
+         <Button disabled={isPendng} onClick={() => handleLogin("google")}>
             Sign in with Google
          </Button>
       </div>
